6630e31e186c82b792118e2925598ba5e2ad6266
whosblue <108531644+whosblue@users.noreply.github.com>
2024-04-28 05:38:03 -0400

    Update MainWindow.xaml.cs
    - Improved error handling (all possible user errors accounted for)
    - Faster RemoveIllegalCharacters() method
    - Added a good bit of comments

--- YouTube Video Downloader 2/YouTube Video Downloader 2/MainWindow.xaml.cs
+++ YouTube Video Downloader 2/YouTube Video Downloader 2/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Diagnostics;
+using System.Diagnostics;
 using System.IO.Packaging;
 using System.IO;
 using System.Reflection.Emit;
@@ -29,18 +29,24 @@ namespace YouTube_Video_Downloader_2
         }
 
         bool downloadAudioOnly = false;
-
-        String RemoveIllegalCharacters(String dirtyString) // Remove illegal characters from video name
+        // Illegal characters are characters not allowed by the file system. For instance, you can't name a file with the character / in it.
+        String RemoveIllegalCharacters(string dirtyString)
         {
-            char[] invalidChars = Path.GetInvalidFileNameChars();
-            foreach (char invalidChar in invalidChars)
+            var invalidCharSet = new HashSet<char>(Path.GetInvalidFileNameChars());
+            var cleanedString = new StringBuilder();
+
+            foreach (char c in dirtyString)
             {
-                dirtyString = dirtyString.Replace(invalidChar.ToString(), "");
+                if (!invalidCharSet.Contains(c))
+                {
+                    cleanedString.Append(c);
+                }
             }
-            return dirtyString;
+
+            return cleanedString.ToString();
         }
 
-        async void DownloadAudio(IStreamInfo streamInfo, YoutubeClient youtube, String title) // Download audio stream and convert to mp3
+        async void DownloadAudio(IStreamInfo streamInfo, YoutubeClient youtube, String title) // Download audio stream and convert to .mp3
         {
             var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
 
@@ -49,7 +55,7 @@ namespace YouTube_Video_Downloader_2
             Directory.CreateDirectory("Downloads\\Audio");
 
             label1.Content = "Downloading..."; // Update the user
-            String filePath = $"Downloads\\Audio\\{RemoveIllegalCharacters(title)}";
+            String filePath = $"Downloads\\Audio\\{RemoveIllegalCharacters(title)}"; // We remove illegal characters otherwise the program will crash.
 
             await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{filePath}.{streamInfo.Container}");
             label1.Content = "Converting to MP3...";
@@ -73,39 +79,60 @@ namespace YouTube_Video_Downloader_2
             // Get the actual stream
             var stream = await youtube.Videos.Streams.GetAsync(streamInfo);
             label1.Content = "Downloading..."; // Update the user
-            // Create directories
+            // Create directories if they don't already exist
             Directory.CreateDirectory("Downloads");
             Directory.CreateDirectory("Downloads\\Videos");
 
             //Download the video
-            String filePath = $"Downloads\\Videos\\{RemoveIllegalCharacters(title)}";
+            String filePath = $"Downloads\\Videos\\{RemoveIllegalCharacters(title)}"; // We remove illegal characters otherwise the program will crash.
             await youtube.Videos.Streams.DownloadAsync(streamInfo, $"{filePath}.{streamInfo.Container}");
 
             Process.Start("explorer.exe", $"Downloads\\Videos"); // Open the Windows File Explorer into the download directory.
             label1.Content = "Video completed. Opened File Explorer. Enjoy!";
         }
 
-        async void DownloadYouTubeVideo(String youtubeVideoURL)
+        bool userHasSelectedDownloadOption = false;
+        async void DownloadYouTubeVideo(String youtubeVideoURL) // Determines how we should download the video, given the user settings. Also grabs & updates metadata.
         {
             var youtube = new YoutubeClient();
             label1.Content = "Loading...";
 
+            if (!userHasSelectedDownloadOption)
+            {
+                label1.Content = "You didn't select a download mode! Select one and press the button again.";
+                return;
+            }
+
+            if (youtubeVideoURL == "")
+            {
+                label1.Content = "You didn't input a URL! Try again.";
+                return;
+            }
+
+            var title = "";
             // Gather Metadata
-            var video = await youtube.Videos.GetAsync(youtubeVideoURL);
+            try
+            {
+                var video = await youtube.Videos.GetAsync(youtubeVideoURL);
 
-            var title = video.Title;
-            var author = video.Author.ChannelTitle;
+                title = video.Title;
+                var author = video.Author.ChannelTitle;
 
-            videoNameStat.Content = $"Title: {title}";
-            creatorNameStat.Content = $"Creator: {author}";
-
-            label1.Content = $"Detected video '{title}'";
+                videoNameStat.Content = $"Title: {title}";
+                creatorNameStat.Content = $"Creator: {author}";
+                label1.Content = $"Detected video '{title}'";
+            }
+            catch(Exception e)
+            {
+                label1.Content = "An error occurred. You must have input an invald YouTube link. Try again!";
+                return;
+            }
 
             // Gather available streams (download qualities)
             var streamManifest = await youtube.Videos.Streams.GetManifestAsync(youtubeVideoURL);
 
             // Get and download the highest quality version, depending on what type of download the user wants
-            if (downloadAudioOnly)
+            if (downloadAudioOnly) // If the user wants an .mp3...
             {
                 try
                 {
@@ -117,7 +144,7 @@ namespace YouTube_Video_Downloader_2
                     label1.Content = "Unknown error occurred. Please be sure the link is correct, or contact developer.";
                 }
             }
-            else
+            else if (!downloadAudioOnly)// If the user wants an .mp4...
             {
                 try
                 {
@@ -134,11 +161,13 @@ namespace YouTube_Video_Downloader_2
         private void EnableDownloadAsVideo(object sender, RoutedEventArgs e) // Video download radio button
         {
             downloadAudioOnly = false;
+            userHasSelectedDownloadOption = true;
         }
 
         private void EnableDownloadAsAudio(object sender, RoutedEventArgs e) // Audio download radio button
         {
             downloadAudioOnly = true;
+            userHasSelectedDownloadOption = true;
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
